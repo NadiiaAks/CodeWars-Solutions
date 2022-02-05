3c19d21c6dfdd282609e598ad4bba01dc7b669c1
nadiia_aksonova <nadiakswork@gmail.com>
2022-02-05 12:28:31 +0200

    Convert number to array of numbers

---
+++ Create Phone Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Create_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+            string one = Convert.ToString((myArray[0-2]));
+
+            Console.WriteLine(one);
+
+
+
+
+
+        }
+    }
+}
---
+++ Digitize/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Digitize
+{
+    class Program
+    {
+        // 7 kpu
+        //Given a non-negative integer, return an array / a list of the individual digits in order
+        public static int[] digitize(int n)
+        {
+
+            char[] num = n.ToString().ToCharArray();
+            int[] result = new int[num.Length];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                result[i] = int.Parse(Convert.ToString(num[i]));
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int check = 12345;
+
+            int[] myArray = digitize(check);
+        
+        }
+    }
+}
