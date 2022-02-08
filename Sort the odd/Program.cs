f3d358dce98a28025460da3382ce7011c7501aec
nadiia_aksonova <nadiakswork@gmail.com>
2022-02-08 14:05:47 +0200

    In array sort the odd numbers in ascending order while leaving the even numbers at their original positions

---
+++ Sort the odd/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort_the_odd
+{
+    class Program
+    {
+
+        //6 kpu
+        //You will be given an array of numbers.
+        //You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+        public static int[] SortArray (int[] array)
+        {
+            int[] odd = array.Where(x => x % 2 != 0).OrderBy(x => x).ToArray();
+
+            for (int i = 0, j =0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0 )
+                {
+                    array[i] = odd[j++];
+                }
+            }
+
+            return array;
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] myArray = { 5, 8, 6, 3, 4 };
+
+            int[] check = SortArray(myArray);
+        }
+    }
+}
---
+++ zz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace zz
+{
+    class Program
+    {
+        
+
+
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            switch (x)
+            {
+                case 10: { goto case 12; Console.WriteLine(++x); break; }
+                case 11: { Console.WriteLine( 11); break; }
+                case 12: { Console.WriteLine(x--); break; }
+            }
+
+
+
+        }
+    }
+}
