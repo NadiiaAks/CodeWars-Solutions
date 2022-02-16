cbb75df0c4b15d54499b94e443010366969cb70c
nadiia_aksonova <nadiakswork@gmail.com>
2022-02-16 20:29:19 +0200

    Method for sort numbers

---
+++ Find the unique number/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Find_the_unique_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Smallest Permutation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Smallest_Permutation
+{
+    class Program
+    {
+        //Sort the number, from smallest elements to largest
+        static public int MinPermutation(int n)
+        {
+           
+            string str = n.ToString();
+            int[] myArray = new int[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+               myArray[i] = int.Parse(str[i].ToString());
+            }
+
+            Array.Sort(myArray);
+
+            int value = 0;
+            for (int i = 0; i < myArray.Length; i++)
+{
+                value *= 10;
+                value += myArray[i];
+            }
+
+            return value;
+        }
+
+        static void Main(string[] args)
+        {
+            int my = 29831;
+
+            int check = MinPermutation(my);
+
+            Console.WriteLine(check);
+            
+        }
+    }
+}
