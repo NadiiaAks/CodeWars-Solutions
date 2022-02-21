05550b02291ffb2e5964bfbe60fafc955a5e473a
nadiia_aksonova <nadiakswork@gmail.com>
2022-02-21 20:21:12 +0200

    Create function fib that returns n'th element of Fibonacci sequence

---
+++ Element of Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Element_of_Fibonacci
+{
+    class Program
+    {
+        //7kpu
+        //Create function fib that returns n'th element of Fibonacci sequence 
+        public static int Fib(int n)
+        {
+            int[] fib = new int[n + 2];
+
+            fib[0] = 0;
+            fib[1] = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+
+            int result = fib[n];
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int fib = Fib(7);
+        }
+    }
+}
